d4541d528d002095657b2e372b42959843265ca0
Muhammad Bilal <dluxsoft.tools@gmail.com>
2024-02-16 18:08:46 +0500

    In Progress

--- app/src/main/java/com/example/surahyaseen/fragments/CalenderFragment.kt
+++ app/src/main/java/com/example/surahyaseen/fragments/CalenderFragment.kt
@@ -1,13 +1,17 @@
 package com.example.surahyaseen.fragments
 
+import android.icu.util.IslamicCalendar
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.surahyaseen.R
+import androidx.fragment.app.Fragment
 import com.example.surahyaseen.databinding.FragmentCalenderBinding
+import java.util.Calendar
 
+
+private const val TAG = "CalenderFragmentXX"
 class CalenderFragment : Fragment() {
     private var _calenderBinding: FragmentCalenderBinding? = null
     private val calenderBinding get() = _calenderBinding!!
@@ -18,6 +22,17 @@ class CalenderFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         _calenderBinding = FragmentCalenderBinding.inflate(inflater, container, false)
+
+        val currentCalender = IslamicCalendar.getInstance()
+
+        val islamicYear: Int = currentCalender.get(IslamicCalendar.YEAR)
+        val islamicMonth: Int = currentCalender.get(IslamicCalendar.MONTH)
+        val islamicDay: Int = currentCalender.get(IslamicCalendar.DAY_OF_MONTH)
+
+
+        Log.d(TAG, "onCreateView: $islamicYear $islamicMonth $islamicDay")
+
+
         return calenderBinding.root
     }
 
